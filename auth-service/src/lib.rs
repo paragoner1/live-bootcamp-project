4a3a13b30e6e06da149f495837b881b05fbdc789
paragoner1 <shears_mete8f@icloud.com>
2025-07-21 18:07:37 -0500

    [Sprint 1][Task 5] note: verified all endpoints with 200 OK in Postman

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,10 +1,13 @@
 use std::error::Error;
-use axum::{serve::Serve, Router};
+use std::result::Result;
+use axum::{serve::Serve, Router, http::StatusCode, response::IntoResponse};
+use axum::routing::{IntoMakeService, post};
+use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 // This struct encapsulates our application-related logic.
 pub struct Application {
-    server: Serve<Router, Router>,
+    server: Serve<TcpListener, IntoMakeService<Router>, Router>,
     // address is exposed as a public field
     // so we have access to it in tests.
     pub address: String,
@@ -16,21 +19,46 @@ impl Application {
         // Also, remove the `hello` route.
         // We don't need it at this point!
         let router = Router::new()
-        .nest_service("/", ServeDir::new("assets"));
+            .fallback_service(ServeDir::new("assets"))
+            // Add all other routes
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/logout", post(logout))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/verify-token", post(verify_token));
 
-        let listener = tokio::net::TcpListener::bind(address).await?;
+        let listener = TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
-        let server = axum::serve(listener, router);
+        let server: Serve<TcpListener, IntoMakeService<Router>, Router> = axum::serve(listener, router.into_make_service());
 
         // Create a new Application instance and return it
-        Ok(Self {
-            server,
-            address,
-        })
+        Ok(Self { server, address })
     }
 
     pub async fn run(self) -> Result<(), std::io::Error> {
         println!("listening on {}", &self.address);
         self.server.await
     }
+}
+
+// Route handlers
+// For now we will simply return a 200 (OK) status code.
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
